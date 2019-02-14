e9f9d116c60dbb2b51269495acfb7be11618cf22
borkanee <grubesic.boris@gmail.com>
2019-02-14 15:26:56 +0100

    Adds routes for PUT and DELETE song

--- routes/songs.js
+++ routes/songs.js
@@ -39,4 +39,40 @@ module.exports = server => {
       return next(new errors.InternalError(err.message))
     }
   })
+
+  // Get one specific song
+  server.get('/songs/:id', async (req, res, next) => {
+    try {
+      let song = await Song.findById(req.params.id)
+      res.send(song)
+      next()
+    } catch (err) {
+      next(new errors.ResourceNotFoundError('No song with id: ' + req.params.id))
+    }
+  })
+
+  // Update one song
+  server.put('/songs/:id', async (req, res, next) => {
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError("Expects 'application/json'"))
+    }
+
+    try {
+      let song = await Song.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      res.send(200, song)
+      next()
+    } catch (err) {
+      next(new errors.ResourceNotFoundError('No song with id: ' + req.params.id))
+    }
+  })
+
+  // Delete one song
+  server.del('/songs/:id', async (req, res, next) => {
+    try {
+      let song = await Song.findByIdAndRemove(req.params.id)
+      res.send(200, song)
+    } catch (err) {
+      next(new errors.ResourceNotFoundError('No song with id: ' + req.params.id))
+    }
+  })
 }
