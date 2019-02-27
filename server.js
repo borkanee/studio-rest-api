5bc7403655ddde2e3f3b859a66def669f6e7ee99
borkanee <grubesic.boris@gmail.com>
2019-02-27 13:29:11 +0100

    testing redirect on heroku

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv').config()
 }
 
+const httpsRedirect = function (req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + req.headers.host + req.url)
+    } else {
+      return next()
+    }
+  } else {
+    return next()
+  }
+}
+
 const server = restify.createServer()
 
 server.use(restify.plugins.bodyParser())
+server.use(httpsRedirect)
 // server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/users', '/authenticate', '/'] }))
 
 server.listen(process.env.PORT, () => {
